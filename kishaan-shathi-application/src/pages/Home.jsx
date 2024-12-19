b58db5db478609017cb227ba9981cf64cbbad279
ShreyaSuddala <shreyasuddala27@gmail.com>
2024-12-19 10:55:09 +0530

    changes

--- kishaan-shathi-application/src/pages/Home.jsx
+++ kishaan-shathi-application/src/pages/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-// import Base from '../component/Base'
+
 
 const Home = () => {
   return (
-    // <Base>
+    
 <div>Home</div>
-    // </Base>
+   
     
   )
 }
--- kishaan-shathi-application/src/pages/Signup.jsx
+++ kishaan-shathi-application/src/pages/Signup.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
-// import Base from '../component/Base'
+
 
 const Signup = () => {
     return (
-        <>
-            {/* <Base> */}
+       
                 <div>Signup</div>
-            {/* </Base> */}
-        </>
+       
     )
 }
 
